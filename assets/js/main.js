1e469968125ac90b370659468faf65a86fd51101
Raju Ahmed <rajuahmedju2020@gmail.com>
2022-09-25 15:57:36 +0600

    Chrismats Website

---
+++ assets/js/main.js
@@ -0,0 +1,74 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/*=============== NEW SWIPER 
+let newSwiper = new Swiper(".new-swiper", {
+    spaceBetween: 24,
+    loop: 'true',
+    slidesPerView: "auto",
+    centeredSlides: true,
+    
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+    breakpoints: {
+        992: {
+          spaceBetween: 80,
+        },
+    },
+});
+
+===============*/
+let newSwiper = new Swiper(".new-swiper", {
+  loop: 'true',
+  spaceBetween: 24,
+  slidesPerView: "auto",
+  centeredSlides: true,
+
+        pagination: {
+          el: ".swiper-pagination",
+          dynamicBullets: true,
+  },
+  breakpoints: {
+        992: {
+          spaceBetween: 80,
+        },
+  },
+});
+
+/*=============== SHOW SCROLL UP ===============*/ 
+function scrollUp(){
+  const scrollUp = document.getElementById('scroll-up');
+  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if(this.scrollY >= 350) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
